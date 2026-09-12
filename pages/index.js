3c749c53c1584f5d5bfa1c5094aebc9919cb4e29
uskko Aimwata <uskkoaimwata@gmail.com>
2026-09-12 10:36:43 +0200

    Create Home component in index.js
    Add Home component to render HTML content.

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: `
+
+      PASTE YOUR ENTIRE HTML CONTENT HERE (everything between <body> and </body>)
+
+    ` }} />
+  )
+}
