651567d2e6069f1fed2431701f46cce4209806e7
uskko Aimwata <uskkoaimwata@gmail.com>
2026-09-13 21:18:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -124,6 +124,7 @@ export default function Home() {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>La Coastal Guesthouse | Walvis Bay, Namibia</title>
 <meta name="description" content="La Coastal Guesthouse in Kuisebmund, Walvis Bay, Namibia. Check room availability, contact us and get directions.">
+<link rel="icon" type="image/png" href="/favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,400;0,9..144,500;0,9..144,600;1,9..144,400;1,9..144,500&family=Public+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
